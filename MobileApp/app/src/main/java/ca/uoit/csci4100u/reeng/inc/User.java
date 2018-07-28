343e06e1ad8251aeff070d181d01127128201b05
BrianJayD <brian.jay1795@gmail.com>
2018-07-28 19:47:14 -0400

    [Added/Modified] Folder for classes.

---
+++ MobileApp/app/src/main/java/ca/uoit/csci4100u/reeng/inc/User.java
@@ -0,0 +1,12 @@
+package ca.uoit.csci4100u.reeng.inc;
+
+/**
+ * Created by brianjayd on 2018-07-28.
+ */
+
+public class User {
+    private String username;
+    private String password;
+    
+
+}
