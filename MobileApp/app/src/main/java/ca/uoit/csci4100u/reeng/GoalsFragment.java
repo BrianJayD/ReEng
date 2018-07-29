02fb8c74da7786d39e555d919d4da9af1bdf19b8
BrianJayD <brian.jay1795@gmail.com>
2018-07-29 11:29:10 -0400

    [Added] Fabricated information.

--- MobileApp/app/src/main/java/ca/uoit/csci4100u/reeng/GoalsFragment.java
+++ MobileApp/app/src/main/java/ca/uoit/csci4100u/reeng/GoalsFragment.java
@@ -2,24 +2,21 @@ package ca.uoit.csci4100u.reeng;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-import static ca.uoit.csci4100u.reeng.MainActivity.mNavigationView;
-
-public class EventFragment extends Fragment {
-    protected static final String TAG = EventFragment.class.getSimpleName();
+public class GoalsFragment extends Fragment {
+    protected static final String TAG = GoalsFragment.class.getSimpleName();
 
     private OnFragmentInteractionListener mListener;
     private MenuItem selectedItem;
 
-    public EventFragment(){
+    public GoalsFragment(){
 
     }
 
@@ -38,7 +35,7 @@ public class EventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_goals, container, false);
         //selectedItem = mNavigationView.getMenu().getItem(0);
         //selectedItem.setChecked(true);
         getActivity().setTitle(R.string.events);
