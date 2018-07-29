623a149bf00b2668ed501ab3ed08871eb6b2b173
BrianJayD <brian.jay1795@gmail.com>
2018-07-29 00:49:08 -0400

    [Added/Modified] Starting functionality. NOTE: Sign In Button Changes Intents.

---
+++ MobileApp/app/src/main/java/ca/uoit/csci4100u/reeng/ArticleFragment.java
@@ -0,0 +1,10 @@
+package ca.uoit.csci4100u.reeng;
+
+import android.app.Fragment;
+
+/**
+ * Created by brianjayd on 2018-07-29.
+ */
+
+public class ArticleFragment extends Fragment {
+}
--- MobileApp/app/src/main/java/ca/uoit/csci4100u/reeng/MainActivity.java
+++ MobileApp/app/src/main/java/ca/uoit/csci4100u/reeng/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.uoit.csci4100u.reeng;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,27 +20,26 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_login);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //Button Floating
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        toggle.syncState();*/
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        /*NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);*/
     }
 
     @Override
@@ -97,5 +97,11 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void signIn(View view) {
+        Intent intent = new Intent(MainActivity.this, FrontPageActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
